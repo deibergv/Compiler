c040fcb75d226b4e9f68e241f27e5df032eeddd0
Deiber <deibergv@gmail.com>
2018-08-28 13:38:28 -0600

    Base de Cup implementada

--- language/jflex/Scanner.flex
+++ language/jflex/Scanner.flex
@@ -1,173 +1,117 @@
+
+/* --------------------------Codigo de Usuario----------------------- */
 package com.language.parser;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
+import java_cup.runtime.*;
+import java.io.Reader;
 
-//clase de los token devueltos
-class Yytoken {
-    Yytoken (int numToken,String token, String tipo, int linea, int columna){
-        //Contador para el número de tokens reconocidos
-        this.numToken = numToken;
-        //String del token reconocido
-        this.token = new String(token);
-        //Tipo de componente léxico encontrado
-        this.tipo = tipo;
-        //Número de linea
-        this.linea = linea;
-        //Columna donde empieza el primer carácter del token
-        this.columna = columna;
-    }
-    //Métodos de los atributos de la clase
-    public int numToken;
-    public String token;
-    public String tipo;
-    public int linea;
-    public int columna;
-    //Metodo que devuelve los datos necesarios que escribiremos en un archive de salida
-    public String toString() {
-        return "Token #"+numToken+": "+token+" C.Lexico: "+tipo+" ["+linea
-        + "," +columna + "]";
-    }
-}
-
-/* Seccion de opciones y declaraciones de JFlex */
 %% //inicio de opciones
-//Cambiamos el nombre la funcion para el siguiente token por nextToken
-%function nextToken
-//Clase publica
-%public
-//Cambiamos el nombre de la clase del analizador
+
+/* ------ Seccion de opciones y declaraciones de JFlex -------------- */
+
+/*
+    Cambiamos el nombre de la clase del analizador a Lexer
+*/
 %class Scanner
-//Agregamos soporte a unicode
-%unicode
-//Codigo java
-%{
 
-    private int contador;
-    private ArrayList<Yytoken> tokens;
-
-	private void writeOutputFile() throws IOException{
-			String filename = "tokens.out";
-			BufferedWriter out = new BufferedWriter(
-				new FileWriter(filename));
-            System.out.println("\n*** Tokens guardados en archivo ***\n");
-			for(Yytoken t: this.tokens){
-				System.out.println(t);
-				out.write(t + "\n");
-			}
-			out.close();
-	}
-%}
-//Creamos un contador para los tokens
-%init{
-    contador = 0;
-	tokens = new ArrayList<Yytoken>();
-%init}
-//Cuando se alcanza el fin del archivo de entrada
-%eof{
-	try{
-		this.writeOutputFile();
-        System.exit(0);
-	}catch(IOException ioe){
-		ioe.printStackTrace();
-	}
-%eof}
-//Activar el contador de lineas, variable yyline
+/*
+    Activar el contador de lineas, variable yyline
+    Activar el contador de columna, variable yycolumn
+*/
 %line
-//Activar el contador de columna, variable yycolumn
 %column
-//Fin de opciones
 
-//Expresiones regulares
-//Declaraciones
-EXP_ALPHA=[A-Za-z]
-EXP_DIGITO=[0-9]
-EXP_ALPHANUMERIC={EXP_ALPHA}|{EXP_DIGITO}
-NUMERO=({EXP_DIGITO})+
-IDENTIFICADOR={EXP_ALPHA}({EXP_ALPHANUMERIC})*
-ESPACIO=" "
-SALTO=\n|\r|\r\n
-//fin declaraciones
+/*
+   Activamos la compatibilidad con Java CUP para analizadores
+   sintacticos(parser)
+*/
+%cup
 
-/* Seccion de reglas lexicas */
-%%
-//Regla     {Acciones}
+/*
+    Declaraciones
 
-{NUMERO}    {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"num",yyline,yycolumn);
-    tokens.add(t);
-    return t;
+    El codigo entre %{  y %} sera copiado integramente en el
+    analizador generado.
+*/
+%{
+    /*  Generamos un java_cup.Symbol para guardar el tipo de token
+        encontrado */
+    private Symbol symbol(int type) {
+        return new Symbol(type, yyline, yycolumn);
+    }
+
+    /* Generamos un Symbol para el tipo de token encontrado
+       junto con su valor */
+    private Symbol symbol(int type, Object value) {
+        return new Symbol(type, yyline, yycolumn, value);
+    }
+%}
+
+
+/*
+    Macro declaraciones
+
+    Declaramos expresiones regulares que despues usaremos en las
+    reglas lexicas.
+*/
+
+/*  Un salto de linea es un \n, \r o \r\n dependiendo del SO   */
+Salto = \r|\n|\r\n
+
+/* Espacio es un espacio en blanco, tabulador \t, salto de linea
+    o avance de pagina \f, normalmente son ignorados */
+Espacio     = {Salto} | [ \t\f]
+
+/* Una literal entera es un numero 0 oSystem.out.println("\n*** Generado " + archNombre + "***\n"); un digito del 1 al 9
+    seguido de 0 o mas digitos del 0 al 9 */
+Entero = 0 | [1-9][0-9]*
+
+
+%% //fin de opciones
+/* -------------------- Seccion de reglas lexicas ------------------ */
+
+/*
+   Esta seccion contiene expresiones regulares y acciones.
+   Las acciones son código en Java que se ejecutara cuando se
+   encuentre una entrada valida para la expresion regular correspondiente */
+
+   /* YYINITIAL es el estado inicial del analizador lexico al escanear.
+      Las expresiones regulares solo serán comparadas si se encuentra
+      en ese estado inicial. Es decir, cada vez que se encuentra una
+      coincidencia el scanner vuelve al estado inicial. Por lo cual se ignoran
+      estados intermedios.*/
+
+<YYINITIAL> {
+
+    /* Regresa que el token SEMI declarado en la clase sym fue encontrado. */
+    ";"                { return symbol(sym.SEMI); }
+    /* Regresa que el token OP_SUMA declarado en la clase sym fue encontrado. */
+    "+"                {  System.out.print(" + ");
+                          return symbol(sym.OP_SUMA); }
+    /* Regresa que el token OP_SUMA declarado en la clase sym fue encontrado. */
+    "-"                {  System.out.print(" - ");
+                          return symbol(sym.OP_RESTA); }
+    /* Regresa que el token OP_SUMA declarado en la clase sym fue encontrado. */
+    "*"                {  System.out.print(" * ");
+                          return symbol(sym.OP_MULT); }
+    /* Regresa que el token PARENIZQ declarado en la clase sym fue encontrado. */
+    "("                {  System.out.print(" ( ");
+                          return symbol(sym.PARENIZQ); }
+                          /* Regresa que el token PARENIZQ declarado en la clase sym fue encontrado. */
+    ")"                {  System.out.print(" ) ");
+                          return symbol(sym.PARENDER); }
+
+    /* Si se encuentra un entero, se imprime, se regresa un token numero
+        que representa un entero y el valor que se obtuvo de la cadena yytext
+        al convertirla a entero. yytext es el token encontrado. */
+    {Entero}      {   System.out.print(yytext());
+                      return symbol(sym.ENTERO, new Integer(yytext())); }
+
+    /* No hace nada si encuentra el espacio en blanco */
+    {Espacio}       { /* ignora el espacio */ }
 }
-"int"   {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"entero",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-}
-"float"   {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"flotante",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-  }
-"Var"   {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"variable",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-  }
-"Set"   {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"modificacion",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-}
-{IDENTIFICADOR}   {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"id",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-}
-"+="  {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"asigna_suma",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-}
-"+"  {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"suma",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-}
-"="  {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"asigna",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-}
-"-"  {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"resta",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-}
-/*Los metacaracteres es necesario usar la secuencia de escape */
-"\^"  {
-    contador++;
-    Yytoken t = new Yytoken(contador,yytext(),"potencia",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-}
-{ESPACIO} {
- 	//ignorar
-}
-{SALTO} {
-    contador++;
-    Yytoken t = new Yytoken(contador,"","fin_linea",yyline,yycolumn);
-    tokens.add(t);
-    return t;
-}
+
+
+/* Si el token contenido en la entrada no coincide con ninguna regla
+    entonces se marca un token ilegal */
+[^]                    { throw new Error("Caracter ilegal <"+yytext()+">"); }
--- src/com/language/parser/Main.java
+++ src/com/language/parser/Main.java
@@ -1,114 +1,236 @@
+//package com.language.parser;
+//
+//import java.io.BufferedReader;
+//import java.io.File;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//
+///**
+// *
+// * @author deiber
+// */
+//public class Main {
+//
+//    /**
+//     * Muestra un menu para elegir la opcion
+//     * mas conveniente
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//        int opcion = 3;
+//        java.util.Scanner in = new java.util.Scanner(System.in);
+//        do {
+//            System.out.println("Seleccione una opción:");
+//            System.out.println("1) Generar analizador lexico");
+//            System.out.println("2) Ejecutar");
+//            System.out.println("3) Salir");
+//            System.out.print("Opcion: ");
+//            opcion = in.nextInt();
+//            switch (opcion) {
+//                case 1: {
+//                    System.out.println("\n*** Generando ***\n");
+//                    String archLex = "";
+//                    if(args.length > 0){
+//                        System.out.println("\n*** Procesando archivo custom ***\n");
+//                        archLex = args[0];
+//                    }else{
+//                        System.out.println("\n*** Procesando archivo default ***\n");
+//                        archLex = "language/jflex/Scanner.flex";
+//                    }
+////                    String[] entrada = {archLex};
+//                    String[] entrada = {"language/jflex/Scanner.flex"};
+//                    jflex.Main.main(entrada);
+//                    //Movemos el archivo generado al directorio src
+////                    File arch = new File("src/com/language/parser/Scanner.java");
+//                    File arch = new File("language/jflex/Scanner.java");
+//                    if(arch.exists()){
+//                        System.out.println("" + arch);
+//                        Path currentRelativePath = Paths.get("");
+//                        String nuevoDir = currentRelativePath.toAbsolutePath().toString()
+//                                + File.separator + "src" + File.separator 
+//                                + "com" + File.separator + "language" + File.separator 
+//                                + "parser"+ File.separator+arch.getName();
+//                        File archViejo = new File(nuevoDir);
+//                        archViejo.delete();
+//                        if(arch.renameTo(new File(nuevoDir))){
+//                            System.out.println("\n*** Generado ***\n");
+//                            System.out.println("\n*** Saliendo automaticamente ***\n");
+//                            System.exit(0);
+//                        }else{
+//                            System.out.println("\n*** No generado ***\n");
+//                        }
+//                    }else{
+//                        System.out.println("\n*** Codigo no existente ***\n");
+//                    }
+//                    break;
+//                }
+//                case 2: {
+//                    String entrada = "";
+//                    System.out.println("\n*** Ejecutando ***\n");
+//                    if(args.length > 0){
+//                        System.out.println("\n*** Procesando archivo custom ***\n");
+//                        entrada = args[0];
+//                    }else{
+//                        System.out.println("\n*** Procesando archivo default ***\n");
+//                        entrada = "entrada.txt";
+//                    }
+//                    BufferedReader bf = null;
+//                    try {
+//                        bf = new BufferedReader(new FileReader(entrada));
+//                        Scanner a = new Scanner(bf);
+//                        Yytoken token = null;
+//                        do {
+//                            token = a.nextToken();
+//                            System.out.println(token);
+//                        } while (token != null);
+//                    } catch (Exception ex) {
+//                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//                    } finally {
+//                        try {
+//                            bf.close();
+//                        } catch (IOException ex) {
+//                            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//                        }
+//                    }
+//                    System.out.println("\n*** Ejecucion finalizada ***\n");
+//                    break;
+//                }
+//                case 3: {
+//                    System.out.println("Programa finalizado!\nVuelva pronto.");
+//                    break;
+//                }
+//                default: {
+//                    System.out.println("Opcion no valida.");
+//                    break;
+//                }
+//            }
+//        } while (opcion != 3);
+//    }
+//
+//}
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.language.parser;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author deiber
  */
 public class Main {
 
-    /**
-     * Muestra un menu para elegir la opcion
-     * mas conveniente
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int opcion = 3;
-        java.util.Scanner in = new java.util.Scanner(System.in);
-        do {
-            System.out.println("Seleccione una opción:");
-            System.out.println("1) Generar analizador lexico");
-            System.out.println("2) Ejecutar");
-            System.out.println("3) Salir");
-            System.out.print("Opcion: ");
-            opcion = in.nextInt();
-            switch (opcion) {
-                case 1: {
-                    System.out.println("\n*** Generando ***\n");
-                    String archLex = "";
-                    if(args.length > 0){
-                        System.out.println("\n*** Procesando archivo custom ***\n");
-                        archLex = args[0];
-                    }else{
-                        System.out.println("\n*** Procesando archivo default ***\n");
-                        archLex = "language/jflex/Scanner.flex";
-                    }
-//                    String[] entrada = {archLex};
-                    String[] entrada = {"language/jflex/Scanner.flex"};
-                    jflex.Main.main(entrada);
-                    //Movemos el archivo generado al directorio src
-//                    File arch = new File("src/com/language/parser/Scanner.java");
-                    File arch = new File("language/jflex/Scanner.java");
-                    if(arch.exists()){
-                        System.out.println("" + arch);
-                        Path currentRelativePath = Paths.get("");
-                        String nuevoDir = currentRelativePath.toAbsolutePath().toString()
-                                + File.separator + "src" + File.separator 
-                                + "com" + File.separator + "language" + File.separator 
-                                + "parser"+ File.separator+arch.getName();
-                        File archViejo = new File(nuevoDir);
-                        archViejo.delete();
-                        if(arch.renameTo(new File(nuevoDir))){
-                            System.out.println("\n*** Generado ***\n");
-                            System.out.println("\n*** Saliendo automaticamente ***\n");
-                            System.exit(0);
-                        }else{
-                            System.out.println("\n*** No generado ***\n");
-                        }
-                    }else{
-                        System.out.println("\n*** Codigo no existente ***\n");
-                    }
-                    break;
-                }
-                case 2: {
-                    String entrada = "";
-                    System.out.println("\n*** Ejecutando ***\n");
-                    if(args.length > 0){
-                        System.out.println("\n*** Procesando archivo custom ***\n");
-                        entrada = args[0];
-                    }else{
-                        System.out.println("\n*** Procesando archivo default ***\n");
-                        entrada = "entrada.txt";
-                    }
-                    BufferedReader bf = null;
-                    try {
-                        bf = new BufferedReader(new FileReader(entrada));
-                        Scanner a = new Scanner(bf);
-                        Yytoken token = null;
-                        do {
-                            token = a.nextToken();
-                            System.out.println(token);
-                        } while (token != null);
-                    } catch (Exception ex) {
-                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                    } finally {
-                        try {
-                            bf.close();
-                        } catch (IOException ex) {
-                            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                    }
-                    System.out.println("\n*** Ejecucion finalizada ***\n");
-                    break;
-                }
-                case 3: {
-                    System.out.println("Programa finalizado!\nVuelva pronto.");
-                    break;
-                }
-                default: {
-                    System.out.println("Opcion no valida.");
-                    break;
-                }
-            }
-        } while (opcion != 3);
-    }
+	public final static int GENERAR = 1;
+	public final static int EJECUTAR = 2;
+	public final static int SALIR = 3;
 
+	/**
+	 * Es un menu para elegir entre generar el analizador lexico y sintactico, o
+	 * ejecutarlos sobre un archivo de pruebas.
+	 *
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		java.util.Scanner in = new Scanner(System.in);
+		int valor = 0;
+		do {
+			System.out.println("Elija una opcion: ");
+			System.out.println("1) Generar");
+			System.out.println("2) Ejecutar");
+			System.out.println("3) Salir");
+			System.out.print("Opcion: ");
+			valor = in.nextInt();
+			switch (valor) {
+			/*
+			 * Generamos el analizador lexico y sintactico. lcalc.flex contiene la
+			 * definicion del analizador lexico ycalc.cup contiene la definicion del
+			 * analizador sintactico
+			 */
+			case GENERAR: {
+				System.out.println("\n*** Generando ***\n");
+				String archLexico = "";
+				String archSintactico = "";
+				if (args.length > 0) {
+					System.out.println("\n*** Procesando archivos custom ***\n");
+					archLexico = args[0];
+					archSintactico = args[1];
+				} else {
+					System.out.println("\n*** Procesando archivo default ***\n");
+					archLexico = "language/jflex/Scanner.flex";
+					archSintactico = "language/Cup/Parser.cup";
+				}
+				String[] alexico = { archLexico };
+				String[] asintactico = { "-parser", "Parser", archSintactico };
+				jflex.Main.main(alexico);
+				try {
+					java_cup.Main.main(asintactico);
+				} catch (Exception ex) {
+					Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+				}
+				// movemos los archivos generados
+				boolean mvAL = moverArch("language/jflex/Scanner.java");
+				boolean mvAS = moverArch("Parser.java");
+				boolean mvSym = moverArch("sym.java");
+				if (mvAL && mvAS && mvSym) {
+					System.exit(0);
+				}
+				System.out.println("Generado!");
+				break;
+			}
+			case EJECUTAR: {
+				/*
+				 * Ejecutamos el analizador lexico y sintactico sobre un archivo de pruebas.
+				 */
+				String[] archivoPrueba = { "test.txt" };
+				Parser.main(archivoPrueba);
+				System.out.println("Ejecutado!");
+				break;
+			}
+			case SALIR: {
+				System.out.println("Adios!");
+				break;
+			}
+			default: {
+				System.out.println("Opcion no valida!");
+				break;
+			}
+			}
+		} while (valor != 3);
+
+	}
+
+	public static boolean moverArch(String archNombre) {
+		boolean efectuado = false;
+		File arch = new File(archNombre);
+		if (arch.exists()) {
+			System.out.println("\n*** Moviendo " + arch + " \n***");
+			Path currentRelativePath = Paths.get("");
+			String nuevoDir = currentRelativePath.toAbsolutePath().toString() + File.separator + "src" + File.separator
+					+ "com" + File.separator + "language" + File.separator + "parser" + File.separator + arch.getName();
+			File archViejo = new File(nuevoDir);
+			archViejo.delete();
+			if (arch.renameTo(new File(nuevoDir))) {
+				System.out.println("\n*** Generado " + archNombre + "***\n");
+				efectuado = true;
+			} else {
+				System.out.println("\n*** No movido " + archNombre + " ***\n");
+			}
+
+		} else {
+			System.out.println("\n*** Codigo no existente ***\n");
+		}
+		return efectuado;
+	}
 }
